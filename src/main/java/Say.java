a753a734de0ded7bb217e90b3843514554707414
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-04-03 20:32:30 -0300

    Solves for 0 to 999

--- src/main/java/Say.java
+++ src/main/java/Say.java
@@ -1,6 +1,67 @@
 public class Say {
 
     public String say(long number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (number == 0) return "zero";
+
+        var hundreds = number / 100;
+        var tens = number % 100 / 10;
+        var units = number % 10;
+        var sb = new StringBuilder();
+
+        if (hundreds != 0) sb.append(sayHundreds(hundreds));
+        if (tens != 0) {
+            if (!sb.isEmpty()) sb.append(" ");
+            sb.append(sayTens(tens, units));
+        }
+        if (units != 0 && tens != 1) {
+            if (!sb.isEmpty()) sb.append("-");
+            sb.append(sayUnits(units));
+        }
+
+        return sb.toString();
+    }
+
+    private String sayHundreds(long hundreds) {
+        return sayUnits(hundreds) + " hundred";
+    }
+
+    private String sayUnits(long units) {
+        if (units == 1) return "one";
+        if (units == 2) return "two";
+        if (units == 3) return "three";
+        if (units == 4) return "four";
+        if (units == 5) return "five";
+        if (units == 6) return "six";
+        if (units == 7) return "seven";
+        if (units == 8) return "eight";
+        if (units == 9) return "nine";
+        throw new IllegalArgumentException("Non valid unit: " + units);
+    }
+
+    private String sayTens(long tens, long units) {
+        if (tens == 1) return sayTeen(units);
+        if (tens == 2) return "twenty";
+        if (tens == 3) return "thirty";
+        if (tens == 4) return "forty";
+        if (tens == 5) return "fifty";
+        if (tens == 6) return "sixty";
+        if (tens == 7) return "seventy";
+        if (tens == 8) return "eighty";
+        if (tens == 9) return "ninety";
+        throw new IllegalArgumentException("Non valid ten: " + tens);
+    }
+
+    private String sayTeen(long units) {
+        if (units == 0) return "ten";
+        if (units == 1) return "eleven";
+        if (units == 2) return "twelve";
+        if (units == 3) return "thirteen";
+        if (units == 4) return "fourteen";
+        if (units == 5) return "fifteen";
+        if (units == 6) return "sixteen";
+        if (units == 7) return "seventeen";
+        if (units == 8) return "eighteen";
+        if (units == 9) return "nineteen";
+        throw new IllegalArgumentException("Non valid unit: " + units);
     }
 }
--- src/test/java/SayTest.java
+++ src/test/java/SayTest.java
@@ -12,61 +12,51 @@ public class SayTest {
         assertThat(say.say(0)).isEqualTo("zero");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void one() {
         assertThat(say.say(1)).isEqualTo("one");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void fourteen() {
         assertThat(say.say(14)).isEqualTo("fourteen");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void twenty() {
         assertThat(say.say(20)).isEqualTo("twenty");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void twentyTwo() {
         assertThat(say.say(22)).isEqualTo("twenty-two");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void thirty() {
         assertThat(say.say(30)).isEqualTo("thirty");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void ninetyNine() {
         assertThat(say.say(99)).isEqualTo("ninety-nine");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void oneHundred() {
         assertThat(say.say(100)).isEqualTo("one hundred");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void oneHundredTwentyThree() {
         assertThat(say.say(123)).isEqualTo("one hundred twenty-three");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void twoHundred() {
         assertThat(say.say(200)).isEqualTo("two hundred");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void nineHundredNinetyNine() {
         assertThat(say.say(999)).isEqualTo("nine hundred ninety-nine");
