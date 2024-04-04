c1afe1de199f3f3aeabd66549d1eb1279deb14e5
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-04-04 18:04:23 -0300

    Exercise solution

--- src/main/java/Say.java
+++ src/main/java/Say.java
@@ -1,14 +1,51 @@
 public class Say {
 
+    private final static String[] UNITS = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    private final static String[] TENS = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    private final static String[] TEENS = {
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    };
+
     public String say(long number) {
+        if (number < 0 || number > 999_999_999_999L)
+            throw new IllegalArgumentException("Number must be between 0 and 999_999_999_999: " + number);
         if (number == 0) return "zero";
+
+        var sb = new StringBuilder();
+
+        for (long divisor = 1_000_000_000; divisor >= 1; divisor /= 1_000)
+            number = decomposeNumber(number, divisor, sb);
+
+        return sb.toString();
+    }
+
+    private long decomposeNumber(long number, long divisor, StringBuilder sb) {
+        if (number >= divisor) {
+            sb.append(sayUntil999(number / divisor));
+            sb.append(describeScale(divisor));
+            number = number % divisor;
+            if (number != 0) sb.append(" ");
+        }
+        return number;
+    }
+
+    private String describeScale(long divisor) {
+        if (divisor == 1_000_000_000) return " billion";
+        if (divisor == 1_000_000) return " million";
+        if (divisor == 1_000) return " thousand";
+        if (divisor == 1) return "";
+        throw new IllegalArgumentException("No scale for divisor: " + divisor);
+    }
+
+    private String sayUntil999(long number) {
+        if (number == 0) return "";
 
         var hundreds = number / 100;
         var tens = number % 100 / 10;
         var units = number % 10;
         var sb = new StringBuilder();
 
-        if (hundreds != 0) sb.append(sayHundreds(hundreds));
+        if (hundreds != 0) sb.append(sayHundred(hundreds));
         if (tens != 0) {
             if (!sb.isEmpty()) sb.append(" ");
             sb.append(sayTens(tens, units));
@@ -21,47 +58,20 @@ public class Say {
         return sb.toString();
     }
 
-    private String sayHundreds(long hundreds) {
+    private String sayHundred(long hundreds) {
         return sayUnits(hundreds) + " hundred";
     }
 
     private String sayUnits(long units) {
-        if (units == 1) return "one";
-        if (units == 2) return "two";
-        if (units == 3) return "three";
-        if (units == 4) return "four";
-        if (units == 5) return "five";
-        if (units == 6) return "six";
-        if (units == 7) return "seven";
-        if (units == 8) return "eight";
-        if (units == 9) return "nine";
-        throw new IllegalArgumentException("Non valid unit: " + units);
+        return UNITS[(int )units - 1];
     }
 
     private String sayTens(long tens, long units) {
         if (tens == 1) return sayTeen(units);
-        if (tens == 2) return "twenty";
-        if (tens == 3) return "thirty";
-        if (tens == 4) return "forty";
-        if (tens == 5) return "fifty";
-        if (tens == 6) return "sixty";
-        if (tens == 7) return "seventy";
-        if (tens == 8) return "eighty";
-        if (tens == 9) return "ninety";
-        throw new IllegalArgumentException("Non valid ten: " + tens);
+        return TENS[(int) tens - 2];
     }
 
     private String sayTeen(long units) {
-        if (units == 0) return "ten";
-        if (units == 1) return "eleven";
-        if (units == 2) return "twelve";
-        if (units == 3) return "thirteen";
-        if (units == 4) return "fourteen";
-        if (units == 5) return "fifteen";
-        if (units == 6) return "sixteen";
-        if (units == 7) return "seventeen";
-        if (units == 8) return "eighteen";
-        if (units == 9) return "nineteen";
-        throw new IllegalArgumentException("Non valid unit: " + units);
+        return TEENS[(int)units];
     }
 }
--- src/test/java/SayTest.java
+++ src/test/java/SayTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class SayTest {
 
@@ -62,37 +62,31 @@ public class SayTest {
         assertThat(say.say(999)).isEqualTo("nine hundred ninety-nine");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void oneThousand() {
         assertThat(say.say(1_000)).isEqualTo("one thousand");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void oneThousandTwoHundredThirtyFour() {
         assertThat(say.say(1_234)).isEqualTo("one thousand two hundred thirty-four");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void oneMillion() {
         assertThat(say.say(1_000_000)).isEqualTo("one million");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void oneMillionTwoThousandThreeHundredFortyFive() {
         assertThat(say.say(1_002_345)).isEqualTo("one million two thousand three hundred forty-five");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void oneBillion() {
         assertThat(say.say(1_000_000_000)).isEqualTo("one billion");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void nineHundredEightySevenBillionSixHundredFiftyFourThreeHundredTwentyOneThousandOneHundredTwentyThree() {
         assertThat(say.say(987_654_321_123L))
@@ -100,14 +94,12 @@ public class SayTest {
                         " three hundred twenty-one thousand one hundred twenty-three");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void illegalNegativeNumber() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> say.say(-1));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void illegalTooBigNumber() {
         assertThatExceptionOfType(IllegalArgumentException.class)
